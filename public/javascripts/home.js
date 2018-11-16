5a9c9af7e7fe3dbed7ef0a9f0501ce17116a50c5
samyoga <bhattaraisamyoga@gmail.com>
2018-11-16 11:00:26 +0545

    made few edits

--- public/javascripts/home.js
+++ public/javascripts/home.js
@@ -1,15 +1,3 @@
-<<<<<<< HEAD
-||||||| merged common ancestors
-$('#btn').on('click',function(){
-    $('#one').hide('slow')
-    $('#two').show('slow');
-});
-=======
-$('#btn').on('click',function(){
-    $('#one').hide('slow')
-    $('#two').show('slow');
-});
-
 $(window).on('scroll', function(){
     if ($(window).scrollTop()){
         $('nav').addClass('black');
@@ -18,4 +6,4 @@ $(window).on('scroll', function(){
         $('nav').removeClass('black');
     }
 });
->>>>>>> cf9552f21ba090065dc4678bf9e65498acc83a6b
+
